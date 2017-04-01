76598f148bb81737fbe3b16053f90486cc5eacb4
bartek <bartek+github@szabat.com>
2017-04-01 23:00:12 +0200

    refa, extracted myrobot class

---
+++ slave/myrobot.js
@@ -0,0 +1,106 @@
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+
+module.exports = MyRobot;
+
+function MyRobot() {
+  EventEmitter.call(this);
+  this.setup=setup;
+  //this.ledBusy=ledBusy;
+
+  this.update=(()=>{myexit(0);});
+  this.restart=(()=>{myexit(10);});
+  this.kill=(()=>{myexit(20);});
+
+  this.led1_switch=led1_switch;
+  this.led2_switch=led2_switch;
+  this.forward=forward;
+  this.backwards=backwards;
+  this.stop=stop;
+  this.left=left;
+  this.right=right;
+  this.setDirection=setDirection;
+
+  function myexit(code) {
+      robot.setLED(1, 1);
+      robot.setLED(2, 1);
+      broadcast("Server closing in 1 sec. Code: "+code);
+      setTimeout(function(){process.exit(code);}, 1000);
+  }
+
+  function broadcast(message) {
+      process.stdout.write(message+"\n");
+      //this.emit('message', message);
+  }
+}
+
+util.inherits(MyRobot, EventEmitter);
+
+//var util = require('util');
+//var EventEmitter = require('events').EventEmitter;
+
+var RaspiRobot = require("./raspirobot.js").RaspiRobot, // Import the library
+    robot = new RaspiRobot();
+
+function setup() {
+  robot.setup(); // Set up GPIO ports
+  ledBusy();
+}
+
+function ledBusy(){
+    robot.setLED(1, 1); // Turn on LED 1
+    robot.setLED(2, 0); // Turn off LED 2
+}
+
+function motorStop() {
+  robot.setLED(1,0);
+  robot.setLED(2,1);
+  robot.setMotor("left",0);
+  robot.setMotor("right",0);
+}
+
+function onIdle() {
+  motorStop();
+}
+
+var stopScheduledTimeout = setTimeout(motorStop, 250)
+
+function unsheduleStop() {
+  clearTimeout(stopScheduledTimeout);
+}
+
+function resheduleStop() {
+    unsheduleStop();
+    stopScheduledTimeout = setTimeout(motorStop, 250);
+}
+
+function led1_switch() { robot.setLED(1,(robot.getLED(1)?0:1)); }
+function led2_switch() { robot.setLED(2,(robot.getLED(2)?0:1)); }
+function forward() { ledBusy(); robot.setMotor("left",1); robot.setMotor("right",1); resheduleStop(); }
+function backwards() { ledBusy(); robot.setMotor("left",1,1); robot.setMotor("right",1,1); resheduleStop(); }
+function stop() { motorStop(); unsheduleStop();}
+function left() { ledBusy(); robot.setMotor("left",0); robot.setMotor("right",1); resheduleStop(); }
+function right() { ledBusy(); robot.setMotor("left",1); robot.setMotor("right",0); resheduleStop(); }
+
+function setDirection(d) { //x,y
+    if (d.x==0){
+      //prosto
+      if (d.y==0) {
+        motorStop();
+      } else {
+        var reverse = d.y>0?0:1;
+        ledBusy(); robot.setMotor("left",1,reverse); robot.setMotor("right",1,reverse);
+      }
+    } else {
+      //skrecamy
+      ledBusy();
+      if (d.y==0) {
+        //w miejscu
+        robot.setMotor("left",1,d.x<0?1:0); robot.setMotor("right",1,d.x>0?1:0);
+      } else {
+        //w ruchu
+        var dir = d.y==1?0:1;
+        robot.setMotor("left",d.x==d.y?1:0,dir); robot.setMotor("right",d.x!=d.y?1:0,dir);
+      }
+    }
+}
--- slave/raspi-slave.js
+++ slave/raspi-slave.js
@@ -4,56 +4,23 @@ net = require('net');
 // Keep track of the chat clients
 var clients = [];
 
-var RaspiRobot = require("./raspirobot.js").RaspiRobot, // Import the library
-    robot = new RaspiRobot();
+var MyRobot = require("./myrobot.js")
+    myrobot = new MyRobot();
 
-robot.setup(); // Set up GPIO ports
-
-function ledBusy(){
-    robot.setLED(1, 1); // Turn on LED 1
-    robot.setLED(2, 0); // Turn off LED 2
-}
-
-ledBusy();
-
-function motorStop() {
-  robot.setLED(1,0);
-  robot.setLED(2,1);
-  robot.setMotor("left",0);
-  robot.setMotor("right",0);
-}
-
-function onIdle() {
-  motorStop();
-}
-
-var idleTimeout = setTimeout(onIdle, 250)
-
-function resetIdleTimeout() {
-    clearTimeout(idleTimeout);
-    idleTimeout = setTimeout(onIdle, 250);
-}
+myrobot.setup(); // Set up GPIO ports
 
 var keyToFun = {
-    "u": ["update",function() { myexit(0); }],
-    "r": ["restart",function() { myexit(10); }],
-    "k": ["kill",function() { myexit(20); }],
+    "u": ["update",myrobot.update],
+    "r": ["restart",myrobot.restart],
+    "k": ["kill",myrobot.kill],
 
-    "o": ["led1 switch",function() { robot.setLED(1,(robot.getLED(1)?0:1)); }],
-    "p": ["led2 switch",function() { robot.setLED(2,(robot.getLED(2)?0:1)); }],
-    "w": ["forward",function() { ledBusy(); robot.setMotor("left",1); robot.setMotor("right",1); }],
-    "s": ["backwards",function() { ledBusy(); robot.setMotor("left",1,1); robot.setMotor("right",1,1); }],
-    " ": ["stop",function() { motorStop(); }],
-    "a": ["left",function() { ledBusy(); robot.setMotor("left",0); robot.setMotor("right",1); }],
-    "d": ["right",function() { ledBusy(); robot.setMotor("left",1); robot.setMotor("right",0); }]
-}
-
-function myexit(code) {
-    clearTimeout(idleTimeout);
-    robot.setLED(1, 1);
-    robot.setLED(2, 1);
-    broadcast("Server closing in 1 sec. Code: "+code);
-    setTimeout(function(){process.exit(code);}, 1000);
+    "o": ["led1 switch",myrobot.led1_switch],
+    "p": ["led2 switch",myrobot.led2_switch],
+    "w": ["forward",myrobot.forward],
+    "s": ["backwards",myrobot.backwards],
+    " ": ["stop",myrobot.stop],
+    "a": ["left",myrobot.left],
+    "d": ["right",myrobot.right]
 }
 
 net.createServer(function (socket) {
@@ -62,7 +29,6 @@ net.createServer(function (socket) {
   socket.write("Welcome " + socket.name + "\r\n");
   broadcast(socket.name + " joined.", socket);
   socket.on('data', function (data) {
-    resetIdleTimeout();
     //console.log(JSON.stringify(data));
     var action = keyToFun[data.toString().trim()[0]];
     if (action==undefined){
@@ -99,26 +65,7 @@ app.ws('/socket', function(ws, req) {
     broadcast("message received on web socket: "+msg);
 
     var d = JSON.parse(msg);
-    if (d.x==0){
-      //prosto
-      if (d.y==0) {
-        motorStop();
-      } else {
-        var reverse = d.y>0?0:1;
-        ledBusy(); robot.setMotor("left",1,reverse); robot.setMotor("right",1,reverse);
-      }
-    } else {
-      //skrecamy
-      ledBusy();
-      if (d.y==0) {
-        //w miejscu
-        robot.setMotor("left",1,d.x<0?1:0); robot.setMotor("right",1,d.x>0?1:0);
-      } else {
-        //w ruchu
-        var dir = d.y==1?0:1;
-        robot.setMotor("left",d.x==d.y?1:0,dir); robot.setMotor("right",d.x!=d.y?1:0,dir);
-      }
-    }
+    myrobot.setDirection(d);
   });
   ws.on('close', function() {
     //ws.send(msg);
@@ -126,10 +73,9 @@ app.ws('/socket', function(ws, req) {
   });
 });
 
-app.listen(3000, function () {
-  console.log('Web app listening on port 3000. http://127.0.0.1:3000/ !')
+app.listen(2000, function () {
+  console.log('Web app listening on port 2000. http://127.0.0.1:2000/ !')
 })
-
 
 function broadcast(message, sender) {
     clients.forEach(function (client) {
@@ -145,3 +91,5 @@ function broadcast(message, sender) {
     // Log it to the server output too
     process.stdout.write(message+"\n");
 }
+
+myrobot.on('message',broadcast);
